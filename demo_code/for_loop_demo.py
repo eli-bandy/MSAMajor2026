636878e2af54bf819425a5e4f03019c3cb0c232a
eli-bandy <elibandy1209@gmail.com>
2026-06-09 11:34:33 -0500

    Added the files
    input_example
    input_example_improved
    input_example_improved_functions
    for_loop_demo

---
+++ demo_code/for_loop_demo.py
@@ -0,0 +1,13 @@
+def main():
+    # print integers between 0 and 10
+          print("\n\n")
+for x in range(11):
+        print(x)
+  
+
+  # print integers between 5 and 10
+for number in range(5, 11):
+        print(number)
+for number in range (0, 11, 2):
+    print(number)
+main()
