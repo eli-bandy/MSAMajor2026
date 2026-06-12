7168fb4075055a69adf7d5246b1bdb6b14c08eaa
eli-bandy <elibandy1209@gmail.com>
2026-06-12 11:32:32 -0500

    June 12th challenge
    vending_machine.py

--- challenge_code/vending_machine.py
+++ challenge_code/vending_machine.py
@@ -4,38 +4,34 @@
 # Output: once at least 50 cents are inputted, display to the user how much change is owed
 
 # create a function for amount_paid
-def amount_paid():
-        # create a while loop to continue prompting user to input coins until 50 cent threshold is achieved
-    while(True):
-        try:
-            print(f"{amount_owed}")
-            coin = int(input("Insert Coin: "))
-            # if amount_owed <= 0 cents, break
-              # validate that the coin denomination inputted is 1, 5, 10, or 25
-            # if valid denomination used, subtract inputted value and continue loop
-            if coin == 1 or 5 or 10 or 25:
-                amount_owed = 50 - coin
-            # if invalid coin used, reprompt with no error message
-            if coin != 1 or 5 or 10 or 25:
-                continue
-            
-            elif amount_owed > 0 and amount_owed < 50:
-                continue
-        except:
-
-
-
-      
-       
-      
-
-        
-    # return value
 
 # create a main function
-    # make a variable for change_due = amount_paid - 50
-    # print(f"Change Owed: {change_due}")
+def main():
+         # create a while loop to continue prompting user to input coins until 50 cent threshold is achieved
+    amount_owed = 50
+    print("Vending Machine\n ---------")
+    while(True):
+        print(f"Amount Due: {amount_owed}")
+        coin = (input(f"Insert Coin: "))
 
+
+        # if amount_owed <= 0 cents, break
+            # validate that the coin denomination inputted is 1, 5, 10, or 25
+        # if valid denomination used, subtract inputted value and continue loop
+        if coin == "1" or coin == "5" or coin == "10" or coin == "25":
+            amount_owed = amount_owed - int(coin)
+            
+            if amount_owed <= 0:
+                print(f"Change Owed: {amount_owed * -1}")
+                break
+        # if invalid coin used, reprompt with no error message
+        else:
+            continue
+
+
+
+        # make a variable for change_due = amount_paid - 50
+        # print(f"Change Owed: {change_due}")
 # call main function
-        
-        
+print("Amount Due: 50")
+main()
