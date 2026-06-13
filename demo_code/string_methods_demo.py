5e95ac6d49577f6481bb3fa316d2f8d7950f26a7
eli-bandy <elibandy1209@gmail.com>
2026-06-13 10:42:04 -0500

    June 13th
    Added updated string_methods_demo.py

--- demo_code/string_methods_demo.py
+++ demo_code/string_methods_demo.py
@@ -78,6 +78,32 @@ def main():
             continue
     print(f"There are {number_of_dogs} {search_word}(s) in the sentence.")
 
+    # using the split() method
+    print("\nUsing the Split() Method\n-------------")
+    # format: make,model,year,price,engine_size
+    car_info = "Ferrari,F-50,2025,500000,4.8\n"
+    car_data = car_info.split(",")
+    print(f"Car data: {car_data}")
+
+    # get the individual items from the list
+    make = car_data[0]
+    model = car_data[1]
+    year = int(car_data[2])
+    price = float(car_data[3])
+    engine_size = float(car_data[4])
+
+    print(f"{year} {make} {model}")
+    print(f"Price: ${price:,.2f} - Engine Size: {engine_size}")
+
+    print("\nnSubstring Method\n-------------")
+    # find the first comma
+    index_of_comma = car_info.find(",")
+    start_index = 0
+
+    #read all characters up to the first comma
+    # list[start_index: stop_index]
+    make_substring = car_info[start_index: index_of_comma]
+    print(f"Make: {make}")
 
 
 main()
