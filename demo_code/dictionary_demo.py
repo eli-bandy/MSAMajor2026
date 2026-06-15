dc9cc3a869fd04824d94cdefafa4840df9c20af7
eli-bandy <elibandy1209@gmail.com>
2026-06-15 11:32:06 -0500

    June 15th
    Added dictionary_demo.py
    Added list_demo.py

---
+++ demo_code/dictionary_demo.py
@@ -0,0 +1,30 @@
+def main():
+    # the need for dictionaries
+    scores = [55, 75, 87, 82, 91]
+    students = ["Alice", "Bob", "Jerry", "Jane", "Bill"]
+
+    # print the names of the students with their scores
+    print("Students and scores using the lists\n--------------")
+    for index in range (len(scores)):
+        print(f"{students[index]}: {scores[index]}")
+
+    # create a dictionary of names and scores
+    student_scores = {
+        "Alice" : 55,
+        "Bob" : 75,
+        "Jerry" : 87,
+        "Jane" : 82,
+        "Bill" : 91
+    }    
+
+    # print Bob and Janes' scores
+    print("\nPrint Bob and Jane's Scores\n-------------------")
+    print(student_scores["Bob"])
+    print(student_scores["Jane"])
+
+    # print all the data in the students scores dictionary
+    print("\nPrint all Student Data\n-----------------")
+    for student in student_scores:
+        print(f"{student}: {student_scores[student]}")
+
+main()
