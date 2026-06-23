769aa9e82c0e538bdeb88cdd2624c1e704473f1f
eli-bandy <elibandy1209@gmail.com>
2026-06-23 11:28:10 -0500

    June 23rd challenge
    updated student API

--- challenge_code/app.py
+++ challenge_code/app.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request, jsonify
-# import student_generator_v2 as sg
+import student_generator_v2 as sg
 
 # create a flask app object
 app = flask.Flask(__name__)
@@ -8,11 +8,36 @@ app = flask.Flask(__name__)
 # tell the server to reload each time the code changes
 app.config["DEBUG"] = True
 
-# create a route for the home page of the application
+"""
+Function to query the list of student dictionaries based on a search key, and a value
+INPUT: search_key - key in the dictionary we want to check the value of
+       search_value - the value of the key we need to match
+OUTPUT: list of student dictionaries that match the search criteria
+"""
+def search_dictionary_list(search_key, search_value):
+    student_dictionaries = sg.get_student_dictionaries()
+    search_critiera_list = []
+    for student_dict in student_dictionaries:
+        if search_value == student_dictionaries[search_key]:
+            search_critiera_list.append(student_dict)
+
+# create a route/view for the home page of the application
 @app.route('/', methods=['GET'])
 def index():
     return "<h1>Student Data API</h1>"
 
 # create end points for the functions we will create
+# create a route to return all student data
+@app.route('/api/students/all', methods=['GET'])
+def api_all():
+    # get student dictionaries
+    student_dictionaries = sg.get_student_dictionaries()
+    return jsonify(student_dictionaries)
+
+# create a route that returns students in a specific major
+
+# create a route that returns students of a specific class (freshman, sophomore, junior, senior)
+# create a route that returns a specific student by ID number
+
 # run the application
 app.run(debug=True)
--- challenge_code/student_generator_v2.py
+++ challenge_code/student_generator_v2.py
@@ -1,6 +1,11 @@
 from Student import Student
 
-def main():
+"""
+Function top return a list of student objects
+INPUT: none
+OUTPUT: list of student objects
+"""
+def load_students() -> list[Student]:
     # open data file
     data_file = open("students.csv", "r")
 
@@ -23,8 +28,8 @@ def main():
             if len(student_info) != 6:
                 raise Exception(f"Error on line {line_number} of the file. Data has {len(student_info)} items, but should have 6 items.\n")
         except Exception as error:
+            #raise Exception(f"Error on line {line_number} of the file. Data has {len(student_info)} items, but should have 6 items.\n")
             continue
-
         # first_name = student_data[0], last_name = student_data[1], etc.
         first_name = student_info[0]
         last_name = student_info[1]
@@ -42,11 +47,47 @@ def main():
         # append the student to the student info list
         student_info_list.append(student)
 
-     # create a for loop to print students   
-    for student in student_info_list:
-        student.print_student_data()
-
     # close file
     data_file.close
+    return student_info_list
+"""
+Function to convert student objects into student dictionaries
+INPUT: list of student objects
+OUTPUT: list of student dictionaries
+"""
+def student_to_dictionary(list_of_students: list[Student]) -> list[dict]:
+    # create an empty list to store the dictionaries
+    student_dictionary_list = []
 
-main()
+    # loop through the list and write each student's data to a dictionary
+    for student in list_of_students:
+        # create an empty dictionary
+        student_dictionary = {}
+
+        # make entries into the dictionary using the student properties
+        # first name, last name, major, gpa, class, id
+        student_dictionary['first_name'] = student.get_first_name()
+        student_dictionary['last_name'] = student.get_last_name()
+        student_dictionary['major'] = student.get_major()
+        student_dictionary['gpa'] = student.get_gpa()
+        student_dictionary['class'] = student.get_class_level()
+        student_dictionary['id'] = student.get_id_number()
+
+        # append the dictionary to the list of dictionaries
+        student_dictionary_list.append(student_dictionary)
+    # return the list of dictionaries
+    return student_dictionary_list
+
+"""
+Function to get student dictionaries
+INPUT: none
+OUTPUT: a list of student dictionaries
+"""
+def get_student_dictionaries():
+    # get a list of students
+    student_list = load_students()
+
+    # get a list of student dictionaries
+    student_dictionaries = student_to_dictionary(student_list)
+
+    return student_dictionaries
